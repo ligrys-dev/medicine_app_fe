6d8c0ac6450c5d5475ba763db0264870fa4529f6
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-10-13 16:17:15 +0200

    update imports

--- src/views/SingleMedView.tsx
+++ src/views/SingleMedView.tsx
@@ -1,5 +1,5 @@
-import { MedDetails } from '../components/MedDetails';
 import { useParams } from 'react-router-dom';
+import { MedDetails } from 'src/components/MedDetails';
 
 export const SingleMedView = () => {
   const { id } = useParams();
