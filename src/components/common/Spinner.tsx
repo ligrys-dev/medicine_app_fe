2881f0820783a297de0ec0d0ec07d765e7bb7c6b
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-10-15 22:14:43 +0200

    styled spinner

--- src/components/common/Spinner.tsx
+++ src/components/common/Spinner.tsx
@@ -1,1 +1,48 @@
-export const Spinner = () => <p>Wczytywanie...</p>;
+import styled from 'styled-components';
+
+export const Spinner = () => (
+  <Div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </Div>
+);
+
+const Div = styled.div`
+  display: block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  margin: 0 auto;
+
+  div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid navy;
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: navy transparent transparent transparent;
+  }
+  div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
