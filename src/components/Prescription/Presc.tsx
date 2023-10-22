1adb48701fabe964e87c4a15f1d566df799c3185
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-10-22 18:03:56 +0200

    add qrcode to presc

--- src/components/Prescription/Presc.tsx
+++ src/components/Prescription/Presc.tsx
@@ -1,18 +1,37 @@
 import { PrescriptionEntity } from 'types';
 import { DeleteBtn } from '../common/DeleteBtn';
 import { StyledLink } from '../styled/StyledLink';
+import { StyledBtn } from '../styled/StyledBtn';
+import { useState } from 'react';
+import { QRcode } from './QRcode';
 
 interface Props {
   presc: PrescriptionEntity;
   onPrescDelete: (id: string) => void;
 }
 
-export const Presc = ({ presc, onPrescDelete }: Props) => (
-  <li>
-    <div>{presc.prescriptionNumber}</div>{' '}
-    <StyledLink to={presc.id ?? ''} state={presc}>
-      Szczegóły
-    </StyledLink>
-    <DeleteBtn onClick={() => onPrescDelete(presc.id ?? '')}>Usuń</DeleteBtn>
-  </li>
-);
+export const Presc = ({ presc, onPrescDelete }: Props) => {
+  const [showQr, setShowQr] = useState(false);
+
+  return (
+    <>
+      <li>
+        <div>{presc.prescriptionNumber}</div>{' '}
+        <StyledBtn onClick={() => setShowQr(true)}>QR</StyledBtn>
+        <StyledLink to={presc.id ?? ''} state={presc}>
+          Szczegóły
+        </StyledLink>
+        <DeleteBtn onClick={() => onPrescDelete(presc.id ?? '')}>
+          Usuń
+        </DeleteBtn>
+      </li>
+      {showQr ? (
+        <QRcode
+          onCloseQr={() => setShowQr(false)}
+          id={presc.id ?? ''}
+          prescNum={presc.prescriptionNumber}
+        />
+      ) : null}
+    </>
+  );
+};
---
+++ src/components/Prescription/QRcode.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react';
+import { api } from 'src/utils/api';
+import { config } from 'src/utils/config/config';
+import styled from 'styled-components';
+import { StyledBtn } from '../styled/StyledBtn';
+import { Spinner } from '../common/Spinner';
+
+interface Props {
+  id: string;
+  onCloseQr: () => void;
+  prescNum: number;
+}
+
+export const QRcode = ({ id, onCloseQr, prescNum }: Props) => {
+  const [qrcode, setQrcode] = useState<string | null>(null);
+  const [pesel, setPesel] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const res: { qr: string; pesel: string } = await api
+        .get(`${config.apiUrl}/prescription/qr/${id}`)
+        .json();
+
+      setQrcode(res.qr);
+      setPesel(res.pesel);
+    })();
+  }, [id]);
+
+  return (
+    <QR>
+      <h2>
+        Numer recepty: <span>{prescNum}</span>
+      </h2>
+      <h3>
+        PESEL: <span>{pesel ?? <Spinner color="white" />}</span>
+      </h3>
+      <div>
+        {qrcode && (
+          <img
+            src={`${qrcode}`} // Utwórz URL danych dla obrazu base64
+            alt="Kod QR"
+          />
+        )}
+      </div>
+      <StyledBtn onClick={onCloseQr}>Powrót</StyledBtn>
+    </QR>
+  );
+};
+
+const QR = styled.div`
+  position: absolute;
+  top: 20%;
+  bottom: 10%;
+  left: 10%;
+  right: 10%;
+  background: navy;
+  color: white;
+  border: 2px solid;
+  text-align: center;
+  border: 2px solid;
+  border-radius: 0.2rem;
+`;
